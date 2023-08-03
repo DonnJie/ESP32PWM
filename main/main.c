da71334e3268cc372c03f163471c1d6fe74f76a2
Sophon3 <1984934742@qq.com>
2023-08-03 08:59:31 +0800

    test

--- main/main.c
+++ main/main.c
@@ -10,12 +10,8 @@
 
 
 
-
-
 void app_main(void)
 {
-
-    
     Bluetooth_init();
     // 初始化 LEDC 驱动程序库
     ledc_fade_func_install(0);
@@ -24,8 +20,8 @@ void app_main(void)
     xTaskCreate(shtc3_task, "shtc3_task", 2048, NULL, 5, NULL);
     // 创建 LED 任务
     xTaskCreate(led_task, "LED Task", 2048, NULL, 4, NULL); 
-    xTaskCreate(rgb_rmt_task, "RGB_RMT_task", 2048, NULL, 4, NULL); 
-
+    xTaskCreate(rgb_rmt_task, "RGB_RMT_task", 2048, NULL, 4, NULL);
+    printf("hello world \n");
     // vTaskDelay(pdMS_TO_TICKS(10000));
     // printf("test-main\n");
     // printf("test-main\n");
